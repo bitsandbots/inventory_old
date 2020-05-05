b49d3ee90e23663b2b30aee0cf5465e5fcb27226
CoreConduit <bitsandbots@gmail.com>
2020-05-05 09:17:48 -0700

    cleaned it up

--- includes/config.php
+++ includes/config.php
@@ -1,20 +1,12 @@
 <?php
 /*
 |--------------------------------------------------------------------------
-| OWSA-INV V2
+| Inventory Database Configuration
 |--------------------------------------------------------------------------
-| Author: Siamon Hasan
-| Project Name: -INV
-| Version: v2
-| Offcial page: http://pp.com/
-| facebook Page: https://www.facebook.com/pp
-|
-|
-|
 */
   define( 'DB_HOST', 'localhost' );          // Set database host
-  define( 'DB_USER', 'root' );             // Set database user
+  define( 'DB_USER', 'webuser' );             // Set database user
   define( 'DB_PASS', 'p1r4sp' );             // Set database password
-  define( 'DB_NAME', 'oswa_inv' );        // Set database name
+  define( 'DB_NAME', 'inventory' );        // Set database name
 
 ?>
