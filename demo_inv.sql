73fd34c5e91a64327fb24d880f5efdd5cdfb1699
CoreConduit <bitsandbots@gmail.com>
2020-11-22 11:58:52 -0800

    add sku
    add sku

--- demo_inv.sql
+++ demo_inv.sql
@@ -107,6 +107,7 @@ CREATE TABLE `products` (
   `id` int(11) UNSIGNED NOT NULL,
   `name` varchar(255) NOT NULL,
   `description` text NOT NULL,
+  `sku` varchar(100) NULL,
   `location` varchar(255) NOT NULL,
   `quantity` varchar(50) DEFAULT NULL,
   `buy_price` decimal(25,2) DEFAULT NULL,
